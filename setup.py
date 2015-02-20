3a6e9b2a0c5806f0e39bbadd768e9e9cbe503479
Tristan Carel <tristan@cogniteev.com>
2015-02-20 10:21:18 +0100

    Add RST `long_description` to setup.py for nice layout on Pypi.

--- setup.py
+++ setup.py
@@ -2,27 +2,58 @@
 import os
 import sys
 from setuptools import setup
+from textwrap import dedent
 
+NAME = "docker-zabbix-sender"
+GITHUB_ORG_URL = "https://github.com/dockermeetupsinbordeaux"
 ROOT_DIR = os.path.dirname(__file__)
 SOURCE_DIR = os.path.join(ROOT_DIR)
-
-requirements = [
-    'docker-py >= 1.0.0',
-]
 
 exec(open('docker_zabbix_sender/version.py').read())
 
 setup(
-    name="docker-zabbix-sender",
+    name=NAME,
     version=version,
     author="Tristan Carel",
     author_email="tristan.carel@gmail.com",
-    url="https://github.com/dockermeetupsinbordeaux/docker-zabbix-sender",
-    download_url="https://github.com/dockermeetupsinbordeaux/docker-zabbix-sender/tarball/v" + version,
-    description="Push Docker containers information to Zabbix",
+    url= GITHUB_ORG_URL + '/' + NAME,
+    download_url="{0}/{1}/tarball/v{2}".format(GITHUB_ORG_URL, NAME, version),
+    description="Push Docker containers statistics to Zabbix efficiently",
+    long_description=dedent("""
+        Rationale
+        ---------
+        Docker Zabbix Sender delivers a daemon script that push to Zabbix statistics about Docker containers.
+
+        It leverages 2 interesting components:
+
+        - Zabbix maintains a tool titled ``zabbix-sender``.
+          It is meant to push `Zabbix trapper items`_ efficiently.
+
+        - Docker 1.5.0 comes with Docker Remote API version 17, providing a new `stats endpoint`_.
+          It allows the client to subscribe to a live feed delivering a container statistics.
+
+        The daemon script stands in the middle of those 2 components.
+        It collects Docker containers statistics and transforms them in Zabbix trapper events.
+
+        Published metrics
+        -----------------
+        The daemon script already emits a collection of metrics: CPU, memory, and network usage.
+        The module's API provide mechanism to easily extend the list of published events.
+
+        Documentation
+        -------------
+        The stable documentation is available on ReadTheDocs_
+
+        .. _Zabbix trapper items: https://www.zabbix.com/documentation/2.4/manual/config/items/itemtypes/trapper
+        .. _stats endpoint: https://docs.docker.com/reference/api/docker_remote_api_v1.17/#get-container-stats-based-on-resource-usage
+        .. _ReadTheDocs: http://docker-zabbix-sender.readthedocs.org/en/stable/
+    """),
     packages=['docker_zabbix_sender'],
-    install_requires=requirements,
+    install_requires=[
+        'docker-py >= 1.0.0',
+    ],
     zip_safe=False,
+    license="Apache license version 2.0",
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Other Environment',

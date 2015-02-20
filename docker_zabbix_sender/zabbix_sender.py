e7f16ab7276cbb15ada2b5ee0fd23d3720337e45
Tristan Carel <tristan@cogniteev.com>
2015-02-20 11:57:24 +0100

    Remove unused code
    Now `zabbix_sender` is not spawn every time trapper events are emitted.

--- docker_zabbix_sender/zabbix_sender.py
+++ docker_zabbix_sender/zabbix_sender.py
@@ -47,24 +47,6 @@ class ZabbixSenderProcess(subprocess.Popen):
         return cmdline
 
 
-def zabbix_sender(input_file, config_file=None, zabbix_server=None, port=None, with_timestamps=False):
-    """Simple wrapper around `zabbix_sender` utility.
-
-    TODO: definitely implement the --real-time option. this module would pipe metrics to one single zabbix_sender
-    process instead of spawning one process each time.
-    """
-    cmdline = ['zabbix_sender', '--input-file', input_file]
-    if config_file:
-        cmdline.extend(['--config', config_file])
-    if zabbix_server:
-        cmdline.extend(['--zabbix-server', zabbix_server])
-    if port:
-        cmdline.extend(['--port', port])
-    if with_timestamps:
-        cmdline.append('--with-timestamps')
-    LOGGER.info("spawning process: " + " ".join(cmdline))
-    subprocess.check_call(cmdline)
-
 class ZabbixSenderEndPoint(EndPoint):
     def __init__(self, **kwargs):
         """

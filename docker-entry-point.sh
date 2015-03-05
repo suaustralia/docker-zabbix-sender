6272d3f9df23f639a0a8e226574db7a41f16e350
Tristan Carel <tristan.carel@gmail.com>
2015-03-05 07:20:22 +0100

    Remove jenkins references in comments

--- docker-entry-point.sh
+++ docker-entry-point.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Set ZABBIX_HOST if not already done
+# Set ZABBIX_HOST environment variable 
+# to container hostname if not already set.
 export ZABBIX_HOST=${ZABBIX_HOST:-$HOSTNAME}
 
-# if `docker run` first argument start with `-` the user is passing jenkins swarm launcher arguments
+# if `docker run` first argument start with `-`, then
+# the user is passing arguments to docker-zabbix-sender
 if [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
-
   exec docker-zabbix-sender "$@"
 fi
 
-# As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
+# As argument is not related to docker-zabbix-sender,
+# then assume that user wants to run his own process,
+# for example a `bash` shell to explore this image
 exec "$@"

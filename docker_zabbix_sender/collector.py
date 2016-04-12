7e4df8d3309f781da30942d682732d205a0adfc3
Thibaut F <fabre.thibaut@gmail.com>
2016-04-12 16:53:42 +0200

    Update collector.py with author corrective
    https://github.com/dockermeetupsinbordeaux/docker-zabbix-sender/issues/10

--- docker_zabbix_sender/collector.py
+++ docker_zabbix_sender/collector.py
@@ -69,8 +69,13 @@ class ContainerStats(threading.Thread):
         previous_network_tx = 0.0
         start = True
         url = self._docker._url("/containers/{0}/stats".format(self.container))
-        self._response = self._docker._get(url, stream=True)
-        stream = self._docker._stream_helper(self._response, decode=True)
+        try:
+            self._response = self._docker._get(url, stream=True)
+            stream = self._docker._stream_helper(self._response, decode=True)
+            self._logger.info("DOCKER RESPONSE %s", self._response)
+        finally:
+            self._logger.info("FINALLY")
+            self.shutdown()
         try:
             for stats in stream:
                 stats['timestamp']= int(time.time())
